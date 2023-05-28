1d985a601e097f6365b1964841aeebda7538f242
Loyoenock <loyoenock@gmail.com>
2023-05-28 17:36:31 +0300

    floating number

---
+++ data_types/float/calculate.py
@@ -0,0 +1,4 @@
+#Inputs two floats, first and second and adds them
+num1 = float(input("Enter first floating point num "))
+num2 = float(input("Enter second floating point num "))
+print(num1 + num2)
