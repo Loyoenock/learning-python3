2bb8c0bdb161f65cdb1c8108824c0b8cd108d9f1
Loyoenock <loyoenock@gmail.com>
2023-05-31 14:15:18 +0300

    Positional arguement

---
+++ function/shirt.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """ Display shirt size and message """
+    print(f"\nThis T-shirt is size {size} and I love {message}")
+
+
+make_shirt('large', 'python')
+make_shirt( size = 'large', message = 'Python')
