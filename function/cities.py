4eb1a48859920498d771e284e313e9457cbc10d9
Loyoenock <loyoenock@gmail.com>
2023-05-31 15:36:09 +0300

    Return cities and countries

---
+++ function/cities.py
@@ -0,0 +1,9 @@
+def describe_city(city, country):
+    """Display city and it's country"""
+    print(f"{city.title()} is in {country.title()}")
+
+describe_city('entebbe', 'uganda')
+describe_city('narobi', 'kenya')
+describe_city('kigali', 'rwanda')
+describe_city('dodoma', 'tanzania')
+describe_city('cairo', 'egypt')
