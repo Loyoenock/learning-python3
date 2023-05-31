81790f70d667213d3046a7409460e42c9d226716
Loyoenock <loyoenock@gmail.com>
2023-05-31 14:14:18 +0300

    Default value and Position argument

---
+++ function/shirt2.py
@@ -0,0 +1,6 @@
+def make_shirt(message, size = 'Large'):
+    """ Display shirt size and message """
+    print(f"This shirt size is {size}, and I love {message}")
+
+make_shirt('python')
+make_shirt(size = 'medium', message = 'coding in python')
