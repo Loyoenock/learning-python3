ae8dbbd335f58c7bcf60aff4d63324f80e70e2a2
Loyoenock <loyoenock@gmail.com>
2023-06-02 08:05:27 +0300

    Added provision for number of songs in an album

--- function/music_album.py
+++ function/music_album.py
@@ -1,11 +1,17 @@
-def make_album(artist_name, music_title):
-    """Return Dictionary with artist name and music title"""
-    album = {'name': artist_name, 'music': music_title}
+def make_album(artist_name, music_title, song_num=""):
+    """
+        Return Dictionary with artist name and music title
+    
+    """
+    if song_num:
+        album = {'name': artist_name, 'music': music_title, 'songs': song_num}
+    else:
+        album = {'name': artist_name, 'music': music_title}
     return album
 
 musician = make_album('Aaron Joshua', 'Kadosh Ata')
 print(musician)
-musician = make_album('Paul Wilbur', 'Let God Araise')
+musician = make_album('Paul Wilbur', 'Let God Araise', 6)
 print(musician)
 musician = make_album('Aaron Strout', 'He reigns')
 print(musician)
