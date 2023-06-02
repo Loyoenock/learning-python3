8447e5a75b145f4c36f3ae21afea4076bee15828
Loyoenock <loyoenock@gmail.com>
2023-06-02 11:34:51 +0000

    Fixed user name output

--- function/user_albums.py
+++ function/user_albums.py
@@ -1,23 +1,25 @@
 def make_album(artist_name, music_title, song_num=""):
     """
-        Return Dictionary with artist name and music title
-    
+    Return Dictionary with artist name, music title, and optional song number
     """
     if song_num:
         album = {'name': artist_name, 'music': music_title, 'songs': song_num}
     else:
         album = {'name': artist_name, 'music': music_title}
     return album
+
 while True:
-    print("\Please Enter Artist name: ")
+    print("\nPlease Enter Artist name:")
     print("(Input 'q' to quit at anytime)")
 
     author = input("Artist name: ")
     if author == 'q':
         break
-    album_title = input("Please enter album title ")
+
+    album_title = input("Please enter album title: ")
     if album_title == 'q':
         break
 
-user_album = make_album(author, album_title)
-print(f"\nUser Album: {user_album}")
+    user_album = make_album(author, album_title)
+    print(f"\nUser Album: {user_album}")
+
