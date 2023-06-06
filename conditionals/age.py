a8e0ac2416172b2041445dee058f5f21205f77ac
Loyoenock <loyoenock@gmail.com>
2023-06-06 07:08:22 +0000

    Multiple test using and/or

---
+++ conditionals/age.py
@@ -0,0 +1,13 @@
+""" Check mulptiple conditions using and """
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 and age_1 >= 21)
+age_1 = 21
+print(age_0 >= 21 and age_1 >= 21)
+
+"""Check  for multitple conditions using or"""
+age_0 = 22
+age_1 = 18
+print(age_0 >= 21 or age_1 >= 21)
+age_0 = 20
+print(age_0 >= 21 or age_1 >= 21)
