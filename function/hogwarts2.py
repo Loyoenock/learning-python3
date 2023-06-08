ed4e119762ffe7addf892ad7b2e3c9d39cceb262
Loyoenock <loyoenock@gmail.com>
2023-06-08 13:22:09 +0000

    File to print individual hall of residence from a dictionary

---
+++ function/hogwarts2.py
@@ -0,0 +1,11 @@
+"""Print student and their corresponding Hall from the dictionary"""
+students = {
+        "Hermione": "Gryffindor",
+        "Harry": "Gryffindor",
+        "Ron": "Gryffindor",
+        "Draco": "Slytherin"
+        }
+print(students['Hermione'])
+print(students['Harry'])
+print(students['Ron'])
+print(students['Draco'])
