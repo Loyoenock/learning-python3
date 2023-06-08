eb39f43faf323679f4024a1dc014dd70d6b61d47
Loyoenock <loyoenock@gmail.com>
2023-06-08 18:20:59 +0000

    Print 3 row of square with oly one for loop

---
+++ function/square1.py
@@ -0,0 +1,12 @@
+""" Print three row of square(#) without nested loop"""
+
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        print('#' * 3)
+
+
+main()
