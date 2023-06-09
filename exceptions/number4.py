35e7978d5f8c1225ff78bbcb31a22e461823e785
Loyoenock <loyoenock@gmail.com>
2023-06-09 07:04:51 +0000

    using pass in handling errors gracefully

---
+++ exceptions/number4.py
@@ -0,0 +1,20 @@
+"""
+    Prompts the user to enter an integer value for 'x' and prints its value.
+
+    Raises:
+        ValueError: If the input provided by the user is not an integer.
+"""
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+def get_int():
+    while True:
+        try:
+            return int(input("What's x? "))
+
+        except ValueError:
+            pass        
+            
+main()
